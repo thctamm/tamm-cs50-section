e7e29c9ae0839e5d21dffa043fdac562447b2bb9
timothy.tamm <tamm@college.harvard.edu>
2016-10-24 10:03:55 -0400

    fix

--- 8/caesar.py
+++
@@ -1,5 +0,0 @@
-word, key = input("enter the word: "), int(input("enter the key: "))
-for c in word:
-    offset = ord('A') if c.isupper() else ord('a')
-    print(chr((((ord(c) + key) - offset) % 26) + offset), end="")
-print()
--- 8/mario.py
+++
@@ -1,8 +0,0 @@
-# Initialize a height variable
-height = -1
-# Ask the user for a height that is within bounds
-while i < 0 or i > 24:
-    height = int(input("how high?"))
-# Print out the pyramid
-for r in range(height):
-    print(" " * (height-r-1) + "#" * (2*r+2))
